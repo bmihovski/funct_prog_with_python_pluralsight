71d6b9885b61af1be4a8247458df6dc5bd211b50
bhristov <magnetgroup@live.com>
2020-02-26 23:05:34 +0200

    Refactoring using composition

--- order.py
+++ order.py
@@ -9,26 +9,47 @@ class Order:
     shipped = False
     customer = None
 
+    @staticmethod
+    def test_expedited(order):
+        return order.expedited
 
-    def get_expedited_order_customer_names(self):
+    @staticmethod
+    def get_customer_name(order):
+        return order.customer.name
+
+    @staticmethod
+    def get_filtered_info(predicate, func):
         output = []
         for order in Order.orders:
-            if order.expedited:
-                output.append(order.customer.name)
-        return output
+            if predicate(order):
+                output.append(func(order))
+        return order
 
+    @staticmethod
+    def get_expedited_order_customer_names():
+        return Order.get_filtered_info(
+            Order.test_expedited,
+            Order.get_customer_name
+        )
 
-    def get_expedited_order_customer_addresses(self):
-        output = []
-        for order in Order.orders:
-            if order.expedited:
-                output.append(order.customer.name)
-        return output
+    @staticmethod
+    def get_customer_address(order):
+        return order.customer.address
 
+    @staticmethod
+    def get_expedited_order_customer_addresses():
+        return Order.get_filtered_info(
+            Order.test_expedited,
+            Order.get_customer_address
+        )
 
-    def get_expedited_order_shipping_addresses(self):
-        output = []
-        for order in Order.orders:
-            if order.expedited:
-                output.append(order.customer.name)
-        return output
+    @staticmethod
+    def get_order_shipping_address(order):
+        return order.shipping_address
+
+    @staticmethod
+    def get_expedited_order_shipping_addresses():
+        return Order.get_filtered_info(
+            Order.test_expedited,
+            Order.get_order_shipping_address
+        )
